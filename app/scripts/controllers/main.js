b1de080de530a8fd5300b34294b24bb8e19662e2
Valentin Vieriu <valentin.vieriu@gmail.com>
2014-01-05 09:06:43 +0100

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularFlickrApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
